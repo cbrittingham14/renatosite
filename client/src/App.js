a1c777a23de3823863516a46d68ad51f4452433e
chizz <cbrittingham@gmail.com>
2020-06-03 14:00:29 -0500

    moved react front end into client folder

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ShopItem from './components/ShopItem';
+
+function App() {
+  return (
+    <div className="App">
+      <ShopItem />
+    </div>
+  );
+}
+
+export default App;
