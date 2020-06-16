012458e902455832fb5c97d24a2ffc0e888ed279
chizz <cbrittingham@gmail.com>
2020-06-16 14:01:07 -0500

    created basic navbar component

---
+++ client/src/components/Nav.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+function Nav() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light">
+      <Link className="navbar-brand" to="/">
+        Shamanic Harmony
+      </Link>
+      <Link className='nav-link' to='#'>Shop</Link>
+      <Link className='nav-link' to='#'>About</Link>
+      <Link className='nav-link' to='#'>My Cart</Link>
+    </nav>
+  )
+  
+};
+
+export default Nav;
