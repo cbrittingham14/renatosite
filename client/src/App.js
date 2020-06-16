46679175c9ba68f2828a2a09189420e7b7c42128
chizz <cbrittingham@gmail.com>
2020-06-16 14:12:48 -0500

    removed unused imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router} from "react-router-dom";
 import './App.css';
 import ShopItem from './components/ShopItem';
 import { StoreProvider } from './utils/GlobalState';
