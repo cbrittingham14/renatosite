e3df62ccacca05b00dbfc9e1163abcd0d5361988
chizz <cbrittingham@gmail.com>
2020-06-18 13:40:55 -0500

    moved routes to separate directory adding necessary code

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const router = require("express").Router();
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 } 
 
-router.use((req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+app.use(routes);
+
 
 const mongooseOptions = {
   useUnifiedTopology: true,
@@ -21,6 +20,6 @@ const mongooseOptions = {
   useFindAndModify: false,
   useCreateIndex: true
 };
-mongoose.connect((process.env.MONGODB_URI || "mongodb://localhost/shamanicDB"), mongooseOptions);
+mongoose.connect((process.env.MONGODB_URI || 'mongodb://localhost/shamanicDB'), mongooseOptions);
 
 app.listen(PORT, () => console.log(`API server listening at http://localhost:${PORT}`));
