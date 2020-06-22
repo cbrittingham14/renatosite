c0a36d59ee374743c8837e402cc0800bf03d0c4a
chizz <cbrittingham@gmail.com>
2020-06-22 13:21:08 -0500

    update global state with shopitems from database, render them and pass props

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,22 +1,34 @@
 import React, { useEffect } from 'react';
-// import { useStoreContext } from '../utils/GlobalState';
+import { useStoreContext } from '../utils/GlobalState';
 import ShopItem from '../components/ShopItem';
 import API from '../utils/API';
 
 
 function MainPage (){
 
-  // const [state, dispatch] = useStoreContext();
+  const [state, dispatch] = useStoreContext();
+  let shopItems = [];
+
+  const setRender =  () => {
+    let itemArray = state.displayItems;
+    if(itemArray) {
+     shopItems = itemArray.map(e => {
+        return <ShopItem props={e} key={e._id} />
+      });
+    }
+  
+  };
 
   useEffect(()=>{
-    API.getAll().then(res => {
-      console.log(res);
+    API.getAll().then(({ data }) => {
+      dispatch({ type: 'set-display', payload: data});
     }).catch(err => console.error(err));
-    API.test().then(res=> console.log('test', res)).catch(err=> console.log(err));
-  })
+  }, []);
 
   return(
     <div>
+      {setRender()}
+      {shopItems}
       <ShopItem />
     </div>
   )
