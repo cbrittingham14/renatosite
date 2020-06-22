9bb6536804ab623bb9f61abb7422b282feb91448
chizz <cbrittingham@gmail.com>
2020-06-22 13:20:15 -0500

    started rendering components with props from mainpage, updated button click to dispatch and update cart state

--- client/src/components/ShopItem.js
+++ client/src/components/ShopItem.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import { useStoreContext } from '../utils/GlobalState';
 
-function ShopItem() {
+function ShopItem( props ) {
 
   const [state, dispatch] = useStoreContext();
 
   const addToCart = (e) => {
-    e.preventDefault();
-    console.log(e.target);
-    dispatch({type: 'add', payload: e.target});
-    console.log('store', state);
+    console.log('e ', e);
+    dispatch({type: 'add', payload: e});
+    console.log('state', state);
   }
 
+  const setRender = (e) => {
+    if(e.props){
+      return(
+        <div className='card' id={e.props._id}>
+          <img src={e.props.imageURL} className='card-img-top' alt='...' />
+          <div className='card-body'>
+            <h5 className='card-title'>{e.props.name}</h5>
+            <p className='card-text'>{e.props.description}</p>
+            <button className='btn btn-primary' onClick={()=> addToCart(e.props._id)}>Add to Cart</ button>
+          </div>
+        </div>
+      )
+    } else {
+      return(
+        <div>NO ITEMS TO SHOW</div>
+      )
+    }
+      
+  };
   return(
-    <div className='card'>
-      <img src='...' className='card-img-top' alt='...' />
-      <div className='card-body'>
-        <h5 className='card-title'>Card title</h5>
-        <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <button className='btn btn-primary' onClick={addToCart}>Add to Cart</ button>
-      </div>
-    </div>
+    <div>{setRender(props)}</div>
   )
 }
 export default ShopItem;
